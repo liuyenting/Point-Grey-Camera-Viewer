0af884a80437e3b99bae64be3bd73c4c76fafe6c
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-12-13 10:37:53 +0800

    Camera can now live preview

--- main.c
+++ main.c
@@ -1,43 +1,49 @@
+#include "camera.h"
+#include "display.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h> // PRIx64
-
-#include "camera.h"
+#include <opencv/highgui.h>
 
 #define IMG_WIDTH   1280
 #define IMG_HEIGHT  960 + 2 // 2 is the magic number.
 #define FRAME_RATE  DC1394_FRAMERATE_30
 
+int capture = 0;
+void sig_handler(int signo) {
+	if(signo == SIGINT)
+		capture = 0; // Stop the acuiqisiton loop.
+}
+
 int main(int argc, char *argv[]) {
+	if(signal(SIGINT, sig_handler) == SIG_ERR)
+		fprintf(stderr, "Unable to catch SIGINT\n");
+
 	dc1394_t *obj;
 	dc1394camera_t *camera;
 	dc1394video_frame_t *frame;
 
 	open_device(&obj, &camera);
 	printf("Using camera with GUID %" PRIx64 "\n", camera->guid);
-
 	set_acq_param(&camera, IMG_WIDTH, IMG_HEIGHT, FRAME_RATE);
 
+	IplImage *img;
+	open_display("Live Preview");
+	fprintf(stderr, "open_display() complete");
+
+	capture = 1;
 	start_capture(&camera);
 
-	char buf[32];
-	FILE *imgfp = NULL;
-
-	for(int i = 1; i <= 50; ++i) {
-		sprintf(buf, "image%d.ppm", i);
-		imgfp = fopen(buf, "wb");
-		fprintf(imgfp, "P6\n%u %u\n255\n", IMG_WIDTH, IMG_HEIGHT);
-
-		grab_frame(&camera, &frame);
-		fwrite(frame->image, 1, frame->image_bytes, imgfp);
-		printf("wrote %s\n", buf);
-		fclose(imgfp);
-
-		dc1394_capture_enqueue(camera, frame);
+	while(capture) {
+		img = grab_frame(&camera, &frame);
+		cvShowImage("Live Preview", img);
+		cvWaitKey(5);
 	}
 
-	stop_capture(&camera);
+	close_display();
 
+	stop_capture(&camera);
 	close_device(&obj, &camera);
 
 	exit(EXIT_SUCCESS);
